c029730544205e1e0059cb5a1955caded9d92121
selikoff <selffmail@gmail.com>
2020-11-18 19:54:30 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andreyselikov
+ * Date: 18.11.2020
+ * Time: 14:51
+ */
+//include('./vendor/autoload.php');
+
+$botKey = '1437468410:AAG5qCp57opoQczctB9UVFgeB8CyshwaGC0';
